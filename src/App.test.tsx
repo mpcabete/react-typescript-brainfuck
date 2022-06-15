14f5181fabea6d7af467d3a75442e1dd63b28582
mp <mateus.cabete@gmail.com>
2022-06-15 15:35:52 -0300

    fix loop

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App, {findBracketPair} from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+test('matchPair function',()=>{
+  let result = findBracketPair('+++[--]',3)
+  expect(result).toBe(6)
+  result = findBracketPair('+++[--',3)
+  expect(result).toBe(-1)
+  result = findBracketPair('+++[[--]-]',3)
+  expect(result).toBe(9)
+  result = findBracketPair('+++[[--]-]',9)
+  expect(result).toBe(3)
+
+})
+//test('renders learn react link', () => {
+//  render(<App />);
+//  const linkElement = screen.getByText(/learn react/i);
+//  expect(linkElement).toBeInTheDocument();
+//});
--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,51 @@
-import React, {  useState } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 const FONT_S = 20
+const TIMEOUT = 20
 const HELLO_WORLD =
-  `>++++++++[<+++++++++>-]<.
->++++[<+++++++>-]<+.
-+++++++..
-+++.
->>++++++[<+++++++>-]<++.
-------------.
->++++++[<+++++++++>-]<+.
-<.
-+++.
-------.
---------.
->>>++++[<++++++++>-]<+.`
+  `
+[ This program prints "Hello World!" and a newline to the screen, its
+  length is 106 active command characters [it is not the shortest.]
+ 
+  This loop is a "comment loop", it's a simple way of adding a comment
+  to a BF program such that you don't have to worry about any command
+  characters. Any ".", ",", "+", "-", "<" and ">" characters are simply
+  ignored, the "[" and "]" characters just have to be balanced.
+]
++++++ +++               Set Cell #0 to 8
+[
+    >++++               Add 4 to Cell #1; this will always set Cell #1 to 4
+    [                   as the cell will be cleared by the loop
+        >++             Add 2 to Cell #2
+        >+++            Add 3 to Cell #3
+        >+++            Add 3 to Cell #4
+        >+              Add 1 to Cell #5
+        <<<<-           Decrement the loop counter in Cell #1
+    ]                   Loop till Cell #1 is zero; number of iterations is 4
+    >+                  Add 1 to Cell #2
+    >+                  Add 1 to Cell #3
+    >-                  Subtract 1 from Cell #4
+    >>+                 Add 1 to Cell #6
+    [<]                 Move back to the first zero cell you find; this will
+                        be Cell #1 which was cleared by the previous loop
+    <-                  Decrement the loop Counter in Cell #0
+]                       Loop till Cell #0 is zero; number of iterations is 8
+ 
+The result of this is:
+Cell No :   0   1   2   3   4   5   6
+Contents:   0   0  72 104  88  32   8
+Pointer :   ^
+ 
+>>.                     Cell #2 has value 72 which is 'H'
+>---.                   Subtract 3 from Cell #3 to get 101 which is 'e'
++++++++..+++.           Likewise for 'llo' from Cell #3
+>>.                     Cell #5 is 32 for the space
+<-.                     Subtract 1 from Cell #4 for 87 to give a 'W'
+<.                      Cell #3 was set to 'o' from the end of 'Hello'
++++.------.--------.    Cell #3 for 'rl' and 'd'
+>>+.                    Add 1 to Cell #5 gives us an exclamation point
+>++.                    And finally a newline from Cell #6
+`
 
 const BF_CHARACTERS = [
 
@@ -26,7 +58,47 @@ const BF_CHARACTERS = [
   '[', //If the byte at the data pointer is zero, then instead of moving the instruction pointer forward to the next command, jump it forward to the command after the matching ] command.
   ']' //If the byte at the data pointer is nonzero, then instead of moving the instruction pointer forward to the next command, jump it back to the command after the matching [ command.
 ]
-const TIMEOUT = 15
+
+export function findBracketPair(code: string, i: number): number {
+  //TODO refactor
+  const char = code[i]
+  if(char == ']'){
+    let closeBrackets = 1
+    while(i >= 0){
+      i--
+      if(code[i] == ']'){
+        closeBrackets ++
+
+      }
+      if(code[i] == '['){
+        closeBrackets --
+        if(closeBrackets == 0){
+          return i
+        }
+      }
+    }
+    return -1
+  }
+  if(char == '['){
+    let openBrackets = 1
+    while(i < code.length-1){
+      i++
+      if(code[i] == '['){
+        openBrackets++
+
+      }
+      if(code[i] == ']'){
+        openBrackets --
+        if(openBrackets == 0){
+          return i
+        }
+      }
+    }
+    return -1
+  }
+  console.log('char', char)
+  return i
+}
 
 function App() {
   const [userCode, setUserCode] = useState(HELLO_WORLD) //we treat code as a state so we can update the clanCode live
@@ -78,15 +150,17 @@ function App() {
         case '[':
           //console.log('memP', mem[p])
           if (mem[p] === 0) {
+            i = findBracketPair(code,i) // Por algum mutivo tinha colocado +1 aki
             //TODO more efficient would be to go in a loop with i backwards of forwards
-            i = code.slice(i).indexOf(']') + i + 1
+            //i = code.slice(i).indexOf(']') + i + 1
             //console.log('forward to',i)
           }
           break
         case ']':
           if (mem[p] !== 0) {
+            i = findBracketPair(code,i)
             //TODO more efficient would be to go in a loop with i backwards of forwards
-            i = code.slice(0, i).lastIndexOf('[')
+            //i = code.slice(0, i).lastIndexOf('[')
             //console.log('backward to',i)
           }
           break
@@ -134,7 +208,7 @@ function App() {
   const codeElement = <span>{Array.from(code).map((c, i) => {
     if (i === executionI) {
       ////console.log('executionI',executionI)
-      return <b style={{fontSize:(FONT_S+3)+'px'}} key={i}>{c}</b>
+      return <b style={{ fontSize: (FONT_S + 3) + 'px' }} key={i}>{c}</b>
     } else {
       return <span key={i}>{c}</span>
     }
